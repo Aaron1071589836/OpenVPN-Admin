f3d49c13d6c4e939a13906123a53180452d82afd
aaronke <1071589836@qq.com>
2020-03-27 12:43:42 +0800

    Update functions.php

--- include/functions.php
+++ include/functions.php
@@ -41,5 +41,34 @@
   function passEqual($pass, $hash) {
     return password_verify($pass, $hash);
   }
-
+/**
+	 * 复制文件夹
+	 * @param $source
+	 * @param $dest
+	 */
+	function copydir($source, $dest){
+		if (!file_exists($dest)) mkdir($dest);
+		$handle = opendir($source);
+		while (($item = readdir($handle)) !== false) {
+			if ($item == '.' || $item == '..') continue;
+			$_source = $source . '/' . $item;
+			$_dest = $dest . '/' . $item;
+			if (is_file($_source)) copy($_source, $_dest);
+			if (is_dir($_source)) copydir($_source, $_dest);
+		}
+		closedir($handle);
+	}
+	function addFileToZip($path,$zip){
+		$handler=opendir($path); //打开当前文件夹由$path指定。
+		while(($filename=readdir($handler))!==false){
+			if($filename != "." && $filename != ".."){//文件夹文件名字为'.'和‘..’，不要对他们进行操作
+				if(is_dir($path."/".$filename)){// 如果读取的某个对象是文件夹，则递归
+					addFileToZip($path."/".$filename, $zip);
+				}else{ //将文件加入zip对象
+					$zip->addFile($path."/".$filename);
+				}
+			}
+		}
+		@closedir($path);
+	}	
 ?>
